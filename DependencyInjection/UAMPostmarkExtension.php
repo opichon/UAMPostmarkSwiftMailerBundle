0618e8404c284865e32912e069a163516d11b69c
Olivier Pichon <op@united-asian.com>
2013-05-29 10:50:00 +0700

    fixed path to services.yml file

--- DependencyInjection/UAMPostmarkExtension.php
+++ DependencyInjection/UAMPostmarkExtension.php
@@ -22,7 +22,7 @@ class UAMPostmarkExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/services'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
         $container->setAlias('uam_postmark', 'uam_postmark.swift_transport.postmark');
